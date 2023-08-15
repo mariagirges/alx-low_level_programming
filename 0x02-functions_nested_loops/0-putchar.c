766249ccf2be011d213a37f30659c418d94e5fe6
mariagirges <mariagirges07@gmail.com>
2023-08-15 07:37:29 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,22 @@
+#include"main.h"
 #include <stdio.h>
+
 /**
- * main-this_function_prints_putchar
- * return0
- */
-int main (void)
+ * main-Entry point
+ *
+ * Description:prints_putchar_using_putchar_prototype
+ *
+ * Return:Always0(Success)
+*/
+
+int main(void)
 {
-	printf("_putchar\n");
-}
-return (0);
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ++ch)
+		_putchar(str[ch]);
+	_putchar('\n');
+
+	return (0);
 }
