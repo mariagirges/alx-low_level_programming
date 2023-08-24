5222e97e16f8fa44179d6a8f590e0e40394365e7
mariagirges <mariagirges07@gmail.com>
2023-08-23 17:53:44 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description:printing digit number
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
