a6762acf6bb178b1bf428582205325ca80ad2e9d
mariagirges <mariagirges07@gmail.com>
2023-08-29 02:28:34 +0300

    6. Chess is mental torture

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,7 +12,14 @@ void print_chessboard(char (*a)[8])
 	for (x = 0; x < 8; x++)
 	{
 		for (y = 0; y < 8; y++)
-			_putchar(a[x][y]);
-		_putchar('\n');
+		{
+			if (y == 7)
+			{
+				_putchar(a[x][y]);
+				_putchar('\n');
+			}
+			else
+				_putchar(a[x][y]);
+		}
 	}
 }
