fcbd7a74629046492bdf0b0360efb0414f2318c1
mariagirges <mariagirges07@gmail.com>
2023-08-30 23:26:55 +0300

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,7 @@
-#ifndef MAIN-H
-#define MAIN-H
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -9,4 +11,5 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
+
 #endif
