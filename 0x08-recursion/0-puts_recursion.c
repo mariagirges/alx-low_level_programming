e6d96309e857550fccd093cf9220a821fcab55f8
mariagirges <mariagirges07@gmail.com>
2023-08-31 20:31:57 +0300

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,9 +7,12 @@
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
+	int i = 0;
+
+	if (str[i] == '\0')
 		return;
-	putchar(%c, *s);
-	_puts_recursion(s + 1);
+
+	putchar("%c", str[i]);
+	_puts_recursion(i + 1);
 	putchar('\n');
 }
