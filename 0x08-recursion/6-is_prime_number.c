6db83b212100fd743faf09f5beb734e81eb7ea61
mariagirges <mariagirges07@gmail.com>
2023-09-01 01:20:11 +0300

    6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 /**
- *prime - return power of any number
+ *prim - return power of any number
  *@x: original number
  *@y: counter that compare multiplication
  *Return: square root
  */
-int prime(int x, int y)
+int prim(int x, int y)
 {
 	if (x == y)
 		return (1);
 	else if (x % y == 0)
 		return (0);
-	return (prime(x, y + 1));
+	return (prim(x, y + 1));
 }
 /**
  * is_prime_number -  returns 1 if the input integer is a prime number
@@ -21,8 +21,7 @@ int prime(int x, int y)
  */
 int is_prime_number(int n)
 {
-	if (prime(n, 1) == 2)
-		return (1);
-	else
+	if (n <= 1)
 		return (0);
+	return (prim(n, 2));
 }
