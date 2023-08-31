f48795c94174ebf52609fb693e31fcf3b7c124c7
mariagirges <mariagirges07@gmail.com>
2023-09-01 01:58:44 +0300

    8. Inception. Now, before you bother telling me it's impossible...

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * wildcmp - compares two strings and returns 1 if the strings can be considered identical
+ * @s1: first string
+ * @s2: second string that can contain the special character *
+ * Return: 1 or 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s1 == '\0' && *(s2 + 1) != '\0' && *s2 == '*')
+		return (0);
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s2 == '*')
+		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	return (0);
+}
