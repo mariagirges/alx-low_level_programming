e46b0e0887697b8c649583a9907ef13ec86eac4b
mariagirges <mariagirges07@gmail.com>
2023-09-06 02:51:55 +0300

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -5,44 +5,38 @@
  * *str_concat - concatenates two strings.
  * @s1: first string
  * @s2: second string
- * Return: pointer should point to a newly 
+ * Return: pointer should point to a newly
  * allocated space in memory or NULL
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	char *str;
-	unsigned int i = 0, n = 0, len = 0, length = 0;
+	int i, n, l, len, length;
+	char *ptr;
 
-	while (s1 && s1[len])
-		len++;
-	while (s2 && s2[length])
-		length++;
+	l = len = 0;
 
-	str = malloc(sizeof(char) * (len + length + 1));
-	if (str == NULL)
+	if (s1 != NULL)
+	{
+		i = 0;
+		while (s1[i++] != '\0')
+			l++;
+	}
+	if (s2 != NULL)
+	{
+		i = 0;
+		while (s2[i++] != '\0')
+			len;
+	}
+	length = l + len;
+	ptr = (char *)malloc(sizeof(char) * (len + 1));
+	if (ptr == NULL)
 		return (NULL);
+	for (i = 0; i < l; i++)
+		ptr[i] = s1[i];
+	for (n = 0; n < len; n++, i++)
+		ptr[i] = s2[n];
+	ptr[length] = '\0';
 
-	i = 0;
-	n = 0;
-
-	if (s1)
-	{
-		while (i < len)
-		{
-			str[i] = s1[i];
-		}
-	}
-	if (s2)
-	{
-		while (i < (len + length))
-		{
-			str[i] = s2[n];
-			i++;
-			n++;
-		}
-	}
-	str[i] = '\0';
-	return (str);
+	return (ptr);
 }
-
