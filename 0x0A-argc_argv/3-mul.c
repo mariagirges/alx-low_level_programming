83cd6d2849f81b44294f80baabc3366aa6420f14
mariagirges <mariagirges07@gmail.com>
2023-09-07 01:50:10 +0300

    3. Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - multiplies two numbers.
+ * @argc: count arguments
+ * @argv: Array Name
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int a;
+
+	if (argc == 1)
+		printf("%d\n", argc - 1);
+	else 
+	{
+		for (a = 0; *argv; i++, argv++)
+			;
+
+		printf("%d\n", a - 1);
+	}
+	return (0);
+}
