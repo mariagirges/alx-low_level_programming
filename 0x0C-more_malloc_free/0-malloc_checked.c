4f898b7a62f02872901a4830525d256e13282595
mariagirges <139780944+mariagirges@users.noreply.github.com>
2023-09-09 00:46:05 +0300

    0. Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *malloc_checked - allocates memory
+ * @b: count bytes
+ * Returns: pointer to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *str;
+
+	str = malloc(b);
+	if (str == NULL)
+		exit(98);
+	return (str);
+}
