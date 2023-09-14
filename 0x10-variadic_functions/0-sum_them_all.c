32d2a868f1b1e5f511f65f644361ae0e216a6e4b
mariagirges <139780944+mariagirges@users.noreply.github.com>
2023-09-14 18:31:00 +0300

    0. Beauty is variable, ugliness is constant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,27 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "variadic_functions.h"
-
 /**
  * sum_them_all - returns the sum of all its parameters.
  * @n: parameter
  * Return: the sum of all its parameters.
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list list;
-	unsigned int i, sum;
-
-	if (n == 0)
-	{
-		return (0);
-	}
+	unsigned int i, sum = 0;
+	
 	va_start(list, n);
 
-	sum = 0;
 	for (i = 0; i < n; i++)
-		sum += va_arg(list, unsigned int);
+		sum += va_arg(list, int);
+
 	va_end(list);
+
 	return (sum);
 }
