4f44e04b27b56b01c0e0f07a1f77c92af9338c10
mariagirges <139780944+mariagirges@users.noreply.github.com>
2023-09-14 13:12:05 +0300

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -4,29 +4,29 @@
 /**
  * get_op_func - finds the correct function
  * @s: he operator passed as argument to the program
- * Return: a pointer to the function that corresponds to the operator 
+ * Return: a pointer to the function that corresponds to the operator
  * given as a parameter.
  */
 int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
 
-    i = 0;
-    while (ops[i].f != NULL)
-    {
-	    if (*s == *(ops[i].op) && s[1] == '\0')
-		    return (ops[i].f);
+	i = 0;
+
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+			return (ops[i].f);
 	    i++;
-    }
+	}
 
-    printf("Error\n");
-    exit(99);
+	return (NULL);
 }
