e418aec4ae3fea916a4fbff908cb3dd6a8b8c14c
mariagirges <139780944+mariagirges@users.noreply.github.com>
2023-09-20 02:51:32 +0300

    6. I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,94 +3,37 @@
 #include "main.h"
 /**
  * **strtow - splits a string into words.
- * @str: string
- * Return: a pointer to an array of strings or NULL
+ * @str: pointer
+ *
+ * Return: char
  */
 char **strtow(char *str)
 {
-	char **ptr;
-int i, n = 0, temp = 0, length = 0, word = num_words(str);
+	char **c;
+	int i;
+	int n = 0;
+	int counter = 0;
 
-if (word == 0)
-return (NULL);
-ptr = (char **) malloc(sizeof(char *) * (word + 1));
-if (ptr != NULL)
-{
-for (i = 0; i <= mblen(str) && word; i++)
-{
-if ((str[i] != ' ') && (str[i] != '\0'))
-length++;
-else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
-{
-ptr[n] = (char *) malloc(sizeof(char) * length + 1);
-if (ptr[n] != NULL)
-{
-while (temp < length)
-{
-ptr[n][temp] = str[(i - length) +temp];
-temp++;
-}
-ptr[n][temp] = '\0';
-length = temp = 0;
-n++;
-}
-else
-{
-while (n-- >= 0)
-free(ptr[n]);
-free(ptr);
-return (NULL);
-}
-}
-}
-ptr[w] = NULL;
-return (ptr);
-}
-else
-return (NULL);
-}
-
-/**
- * num_words - counts the number of words in string
- *@str: string
- *Return: number of words
- */
-int num_words(char *str)
-{
-int i = 0, w = 0;
-
-while (i <= len(str))
-{
-if ((str[i] != ' ') && (str[i] != '\0'))
-{
-i++;
-}
-else if (((str[i] == ' ') || (str[i] == '\0')) && i && (str[i - 1] != ' '))
-{
-w += 1;
-i++;
-}
-else
-{
-i++;
-}
-}
-return (w);
-}
-
-/**
- * len - returns length of string
- *@str: string
- * Return: length of the string
- */
-int len(char *str)
-{
-int length = 0;
-
-if (str != NULL)
-{
-while (str[length])
-length++;
-}
-return (length);
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != 32)
+			counter++;
+	}
+	c = malloc(sizeof(char) * counter);
+	if (c == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != 32)
+		{
+			*c[n] = str[i];
+			n++;
+		}
+		else
+		{
+		}
+	}
+	return (c);
 }
