16a78cc83dc515eb0373dad05061d986b9ee0544
mariagirges <139780944+mariagirges@users.noreply.github.com>
2023-09-20 03:01:34 +0300

    task

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,9 +11,9 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list list;
 	unsigned int i, sum = 0;
-	
+
 	va_start(list, n);
-	
+
 	for (i = 0; i < n; i++)
 		sum += va_arg(list, int);
 	va_end(list);
