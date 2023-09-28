d99ec24e8a8287703dacda6191818db190af1ac4
mariagirges <mariagirges07@gmail.com>
2023-09-28 12:53:52 -0700

    0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,21 +7,20 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i = 0;
+	unsigned int tot, pow;
+	int length;
 
 	if (b == NULL)
 		return (0);
-
-	while (*b != '\0')
+	for (length = 0; b[length]; length++)
 	{
-		i = i << 1;
-
-		if (*b != '1' && *b != '0')
+		if (b[length] != '0' && b[length] != '1')
 			return (0);
-
-		else if (*b == '1')
-			i = i | 1;
-		b++;
 	}
-	return (i);
+	for (pow = 1, tot = 0, length--; length >= 0; length--, pow *= 2)
+	{
+		if (b[length] == '1')
+			tot += pow;
+	}
+	return (tot);
 }
